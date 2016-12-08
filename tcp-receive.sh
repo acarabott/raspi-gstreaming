fe88b1d2dcaa9b31c9678768047aff8ee1273192
Arthur Carabott <arthurc@gmail.com>
2016-12-08 17:36:19 +0000

    fix tcp-receive

--- tcp-receive.sh
+++ tcp-receive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-tcpclientsrc host=$1 port=6666 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! appsink
+gst-launch-1.0 tcpclientsrc host=$1 port=6666 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! appsink
