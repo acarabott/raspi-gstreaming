50172dc458a4b96075d68ead7eaaa161284b9759
Arthur Carabott <arthurc@gmail.com>
2016-12-08 17:37:10 +0000

    fix tcp recv output sink

--- tcp-receive.sh
+++ tcp-receive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-gst-launch-1.0 tcpclientsrc host=$1 port=6666 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! appsink
+gst-launch-1.0 tcpclientsrc host=$1 port=6666 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink
